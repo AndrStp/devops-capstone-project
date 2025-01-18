f296b9b1bf4c45951fa4da69865239bc72b21509
Andrey Stepanenko <ast.university@hotmail.com>
2025-01-18 07:38:15 -0500

    add test case for read account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,12 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should return the account info"""
+        account = self._create_accounts(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        account_data = response.get_json()
+        self.assertEqual(account_data.get("name"), account.name)
